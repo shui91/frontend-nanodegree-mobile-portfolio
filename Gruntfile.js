3113ed638b371b90eaaf81fbddecb5fd6367b1ca
shui91 <sherman.sy.hui@gmail.com>
2015-06-26 11:39:14 -0700

    update: Grunfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,8 @@
-'use strict'
-
-var ngrok = require('ngrok');
-
 module.exports = function(grunt) {
+	'use strict';
+
+	var ngrok = require('ngrok');
+
 	require('load-grunt-tasks')(grunt);
 
 	var config = grunt.file.readYAML('Gruntconfig.yml');
@@ -10,13 +10,13 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		jshint: {
 			all: [
-				'Gruntfile.js',
+				'Gruntfile.js', 'src/**/*.js', 'dist/**/*.js'
 			]
 		},
 		pagespeed: {
 			options: {
 				nokey: true,
-				local: "en_GB",
+				locale: "en_GB",
 				threshold: 40
 			},
 			local: {
@@ -30,13 +30,64 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		cssmin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: 'src/css/',
+					src: '*.css',
+					dest: 'dist/css',
+					ext: '.min.css'
+				}]
+			}
+		},
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: true,
+					minifyCSS: true,
+				},
+				files: {
+					'dist/index.html': 'src/index.html',
+					'dist/project-2048.html': 'src/project-2048.html',
+					'dist/project-mobile.html': 'src/project-mobile.html',
+					'dist/project-webperf.html': 'src/project-webperf.html',
+					'dist/views/pizza.html': 'src/views/pizza.html'
+				}
+			}
+		},
+		uglify: {
+			my_target: {
+				files: {
+					'dist/js/app.min.js': ['src/js/perfmatters.js']
+				}
+			}
+		},
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'src/', 
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'dist/',
+				}]
+			}
+		},
 		watch: {
+			scripts: {
+				files: ['dist/js/*.js', 'src/js/*.js'],
+				tasks: ['jshint']
+			},
+			src: {
+
+			}
 		}
 	});
 
 	grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
 		var done = this.async();
-		var port = 9292;
+		var port = 8080;
 
 		ngrok.connect(port, function(err, url) {
 			if (err !== null) {
@@ -51,6 +102,16 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('default', [
 		'jshint',
-		'psi-ngrok'
+		'psi-ngrok',
+		'htmlmin',
+		'cssmin',
+		'uglify',
+		'imagemin'
 	]);
+
+	grunt.registerTask('minify', [
+		'htmlmin',
+		'cssmin',
+		'uglify',
+		'jshint']);
 };
